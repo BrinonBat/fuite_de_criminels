951e8afa227832031916cfb90f49f978934c0192
BrinonBat <baptiste@brinon.eu>
2020-04-08 17:41:56 +0200

    ajout de Jeu.hh

---
+++ Jeu.hh
@@ -0,0 +1,14 @@
+
+class Jeu{
+	public:
+		//constructeur
+		Jeu(int const & larg,int const & haut);
+
+		//accesseurs
+
+		//méthodes
+		void init(int const & nb_gendarmes,int const & nb_voleurs);
+	private:
+	Case[] plateau;
+
+}
