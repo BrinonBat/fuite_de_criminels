cb345d473b90e2c8bf2775ad08358ebd221fec11
BrinonBat <baptiste@brinon.eu>
2020-04-08 17:42:19 +0200

    ajout d'une section " paramètrage " avec les constantes modifiables

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include "Jeu.hh"
 
 int main(int argc,char * argv[]){
-	Jeu game;
-	int nb_gendarmes = 4
-	int nb_voleurs = 3
+	//paramètrage
+	int hauteur=20;
+	int largeur=20;
+	int nb_gendarmes = 4;
+	int nb_voleurs = 3;
+
+	//initialisation
+	Jeu game=new Jeu(largeur,hauteur);
 	game.init(nb_gendarmes,nb_voleurs);
 
 }
