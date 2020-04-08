03755f131228652ca91bcacd17319537df87c862
BrinonBat <baptiste@brinon.eu>
2020-04-08 16:40:28 +0200

    ajout de Personnage.cc

---
+++ Personnage.cc
@@ -0,0 +1,21 @@
+#include "Personnage.hh"
+
+/**
+* \brief utilisation d'une IA pour déterminer le prochain coup du Voleur
+* \param game état actuel du jeu
+* \return entier correspond au numéro du coup à jouer
+*/
+int Voleur::prevoir_coup(Jeu const & game){
+	return 0;
+	/* mettre l'IA ici */
+}
+
+/**
+* \brief utilisation d'une IA pour déterminer le prochain coup du Gendarme
+* \param game état actuel du jeu
+* \return entier correspond au numéro du coup à jouer
+*/
+int Gendarme::prevoir_coup(Jeu const & game){
+	return 0;
+	/* mettre l'IA ici */
+}
