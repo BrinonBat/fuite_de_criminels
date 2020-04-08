cc8cc3981d97ee3bfc764b8ec96ff30dc8982b78
BrinonBat <baptiste@brinon.eu>
2020-04-08 16:14:19 +0200

    ajout de Personnage, contenant Voleur et Gendarme

---
+++ Personnage.hh
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+
+enum class Cardinal{
+	Nord,
+	Sud,
+	Est,
+	Ouest
+};
+
+class Personnage{
+	public:
+		//Constructeur
+		Personnage(std::string const & nom, Coordonnee const & pos,Cardinal const & dir):
+			nom(nom),position(pos),direction(dir){}
+
+		//accesseurs
+		/* à faire */
+
+		//méthodes
+		virtual int prevoir_coup(Jeu const & game) const;
+
+	private:
+		std::string nom;
+		Coordonnee position;
+		Cardinal direction;
+};
+
+class Voleur:public Personnage{
+	public:
+		int prevoir_coup(Jeu const & game) const override;
+}
+
+class Gendarme:public Personnage{
+	public:
+		int prevoir_coup(Jeu const & game) const override;
+}
