69a69e2ae4ee9a74bb575c6079268b095950d720
BrinonBat <baptiste@brinon.eu>
2020-04-09 16:01:33 +0200

    retrait de cases, on fera tout via coordonnées

--- Jeu.hh
+++ Jeu.hh
@@ -1,14 +1,27 @@
+#include <vector>
+#include "Personnage.hh"
 
 class Jeu{
 	public:
 		//constructeur
-		Jeu(int const & larg,int const & haut);
+		Jeu(int const & larg,int const & haut):
+			largeur(larg),hauteur(haut){};
 
 		//accesseurs
+		int getLargeur() {return largeur;}
+		int getHauteur() {return hauteur;}
 
 		//méthodes
 		void init(int const & nb_gendarmes,int const & nb_voleurs);
+
+		void ajoutVoleur(Position p,Cardinal dir);
+		void ajoutGendarme(Position p,Cardinal dir);
+		void supprimeVoleur(Position p);
+		void supprimeGendarme(Position p);
+
 	private:
-	Case[] plateau;
+		int largeur;
+		int hauteur;
+		Personnage <std::vector> liPersos;
 
 }
