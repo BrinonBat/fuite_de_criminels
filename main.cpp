0849cdb55b7fec0542ebaf32bc3a56239c57bb20
BrinonBat <baptiste@brinon.eu>
2020-04-20 18:05:01 +0200

    test sur le déroulement d'un tour à faire

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main(){
 
 	//creation des entités
 	NonJoueur Sortie(Position(38,38),Type::sortie);
-	Voleur V1(Position(6,5),0.8,"V1");
-	Voleur V2(Position(5,6),0.8,"V2");
-	Gendarme G1(Position(6,5),0.8,"G1");
-	Gendarme G2(Position(6,5),1.0,"G2");
+	Voleur V1(Position(6,6),0.8,"V1");
+	Voleur V2(Position(38,37),0.8,"V2");
+	Gendarme G1(Position(7,6),0.8,"G1");
+	Gendarme G2(Position(6,8),1.0,"G2");
 
 	//ajout des entités au Jeu
 	Test.ajouter_joueur(V1);
@@ -21,4 +21,5 @@ int main(){
 	//tests verifiant une bonne initialisation:
 	std::cout<<"test nbVoleurs résultat attendu: 2 -->"<<Test.getNbVoleurs()<<std::endl;
 	std::cout<<"test nbCaptures résultat attendu: 0 -->"<<Test.getNbCaptures()<<std::endl;
+	std::cout<<"test déplacement résultat attendu: ";
 }
