f6380e257814e8d65f86842fce296c6d6913bfc7
BrinonBat <baptiste@brinon.eu>
2020-04-21 11:06:20 +0200

    constat d'un segfault sur jouer_tour

--- Jeu.cc
+++ Jeu.cc
@@ -2,29 +2,33 @@
 
 void Jeu::initialise(){
 
-
+	//à configurer aprés le GUI
 }
 
 void Jeu::afficher()
 {
 
-
+	//GUI fera le travail
 
 }
 
+// déroulement d'un tour /!\ ça segfault
 void Jeu::Jouer_tour(){
 	for(auto i:Liste_Joueur){
 		deplacement(*i,i->Joue_Deplacement());
 	}
 }
 
+//ajoute un joueur à la partie
 void Jeu::ajouter_joueur(Joueur const &J)
 {
 	Liste_Joueur.push_back(J.clone());
 	if(J.estVoleur()) ajoutUnVoleur();
 }
 
+//retire un voleur de la partie et mets fin à la partie s'il s'agissait du dernier
 void Jeu::supprimer_voleur(unsigned int id){
+	//parcours des voleur et retrait de celui concerné
 	for(auto i =Liste_Joueur.begin(); i!= Liste_Joueur.end();++i){
       if(Liste_Joueur[size_t(*i)]->getId()==id && Liste_Joueur[size_t(*i)]->estVoleur()){
         Liste_Joueur.erase(i);
@@ -35,10 +39,12 @@ void Jeu::supprimer_voleur(unsigned int id){
 	verifieFin();
 }
 
+//ajout d'un objet à la partie
 void Jeu::ajouter_nonJoueur(NonJoueur nJ){
 	Liste_Objets.push_back(new NonJoueur(nJ)); // fonctionnement à confirmer
 }
 
+// execute le déplacement d'un joueur vers une direction donnée
 void Jeu::deplacement(Joueur & J, Direction const & D){
 	//calcul de la nouvelle position
 	J.setPosition(J.getPosition()+D);
@@ -49,7 +55,7 @@ void Jeu::deplacement(Joueur & J, Direction const & D){
 		for(auto sortie : Liste_Objets){
 			if(sortie->getType()==Type::sortie && J.getPosition()==sortie->getPosition()) supprimer_voleur(J.getId());
 		}
-	//traitement du gendarmes
+	//traitement du gendarme
 	}else{
 		for(auto voleur : Liste_Joueur){
 			if(voleur->estVoleur() && J.getPosition()==voleur->getPosition()){
