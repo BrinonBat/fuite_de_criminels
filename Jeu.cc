abce307aa39a5e46fb4660ec09d0017de9a68e21
BrinonBat <baptiste@brinon.eu>
2020-04-21 14:59:57 +0200

    segfault encore, même si c'est dans jouer_tour

--- Jeu.cc
+++ Jeu.cc
@@ -18,6 +18,24 @@ void Jeu::Jouer_tour(){
 	for(auto i:Liste_Joueur){
 		deplacement(*i,i->Joue_Deplacement());
 	}
+	for(auto J:Liste_Joueur){
+		//traitement du voleur
+		if(J->estVoleur()){
+			//verification de la fuite du voleur
+			for(auto sortie : Liste_Objets){
+				if(sortie->getType()==Type::sortie && J->getPosition()==sortie->getPosition()) supprimer_voleur(J->getId());
+			}
+		//traitement du gendarme
+		}else{
+			for(auto voleur : Liste_Joueur){
+				if(voleur->estVoleur() && J->getPosition()==voleur->getPosition()){
+					supprimer_voleur(voleur->getId()); // remplacement du fonctionnement par id par un fonctionnement par voleur ?
+					ajoutUneCapture();
+				} // fin if
+			}// fin for
+		} // fin else
+	}
+
 }
 
 //ajoute un joueur à la partie
@@ -28,19 +46,16 @@ void Jeu::ajouter_joueur(Joueur const &J)
 }
 
 //retire un voleur de la partie et mets fin à la partie s'il s'agissait du dernier
-void Jeu::supprimer_voleur(Joueur & J){
-	int i=0;
-	for(auto && JJ: Liste_Joueur)
-	{
-		if(JJ->getNom()==J.getNom())
-		{
-			delete JJ;
-			break;
-		} 
-		i++;
-	}
-	Liste_Joueur.erase(Liste_Joueur.begin()+i);
-
+void Jeu::supprimer_voleur(unsigned int id){
+	//parcours des voleur et retrait de celui concerné
+	for(auto i =Liste_Joueur.begin(); i!= Liste_Joueur.end();++i){
+      if(Liste_Joueur[size_t(*i)]->getId()==id && Liste_Joueur[size_t(*i)]->estVoleur()){
+        Liste_Joueur.erase(i);
+        return; // fin si le voleur est supprimé
+      }
+    }
+	//on vérifie si c'était le dernier voleur sur le terrain
+	verifieFin();
 }
 
 //ajout d'un objet à la partie
@@ -53,10 +68,11 @@ void Jeu::deplacement(Joueur & J, Direction const & D){
 	//calcul de la nouvelle position
 	J.setPosition(J.getPosition()+D);
 
+/* Tentative de Nicolas
 	Joueur * Joueur_Modifie = &J;
 	// std::cout<<"ADD:"<<&J<<"\n";
 	// ADD:0x7ffccbed6870
-	
+
 
 
 	for(auto && JJ : Liste_Joueur){
@@ -67,31 +83,26 @@ void Jeu::deplacement(Joueur & J, Direction const & D){
 		// ADD:0x56180e1c7fb8
 		if(JJ->getNom()==Joueur_Modifie->getNom()) {JJ->setPosition(J.getPosition());}
 	}
+*/
 
+
+/*
 	//traitement du voleur
 	if(J.estVoleur()){
 		//verification de la fuite du voleur
-		for(auto &&sortie : Liste_Objets){
-			if(sortie->getType()==Type::sortie && J.getPosition()==sortie->getPosition()) {std::cout<<"Un voleur est sortie!\n"; supprimer_voleur(J);}
-		}
-
-		for(auto &&voleur : Liste_Joueur){
-			if(voleur->estVoleur()==false && J.getPosition()==voleur->getPosition()){
-				std::cout<<"Un voleur est capturé!\n";
-				supprimer_voleur(J); // remplacement du fonctionnement par id par un fonctionnement par voleur ?
-				ajoutUneCapture();
-			} // fin if
+		for(auto sortie : Liste_Objets){
+			if(sortie->getType()==Type::sortie && J.getPosition()==sortie->getPosition()) supprimer_voleur(J.getId());
 		}
 	//traitement du gendarme
 	}else{
-		for(auto &&voleur : Liste_Joueur){
+		for(auto voleur : Liste_Joueur){
 			if(voleur->estVoleur() && J.getPosition()==voleur->getPosition()){
-				std::cout<<"Un voleur est capturé!\n";
-				supprimer_voleur(J); // remplacement du fonctionnement par id par un fonctionnement par voleur ?
+				supprimer_voleur(voleur->getId()); // remplacement du fonctionnement par id par un fonctionnement par voleur ?
 				ajoutUneCapture();
 			} // fin if
 		}// fin for
 	} // fin else
+*/
 } // fin méthode
 
 
