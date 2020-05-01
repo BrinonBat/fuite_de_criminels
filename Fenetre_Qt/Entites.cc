c69f3d33cc4a9de1e5889c48a1ff76c32dc6c768
nimartinez <32000194+nimartinez@users.noreply.github.com>
2020-05-01 02:38:36 +0200

    Changement de Hitbox

--- Fenetre_Qt/Entites.cc
+++ Fenetre_Qt/Entites.cc
@@ -36,7 +36,7 @@ bool Entite::Hitbox_touche(Entite &J2)
 	((this->getHitbox().getG()<=J2.getHitbox().getG() and this->getHitbox().getD()>=J2.getHitbox().getG()) && ((this->getHitbox().getB()<=J2.getHitbox().getH() and this->getHitbox().getH()>=J2.getHitbox().getH()) or ((this->getHitbox().getB()<=J2.getHitbox().getB() and this->getHitbox().getH()>=J2.getHitbox().getB()))))
 	or (
 	(this->getHitbox().getG()<=J2.getHitbox().getD() and this->getHitbox().getD()>=J2.getHitbox().getD()) && ((this->getHitbox().getB()<=J2.getHitbox().getH() and this->getHitbox().getH()>=J2.getHitbox().getH()) or ((this->getHitbox().getB()<=J2.getHitbox().getB() and this->getHitbox().getH()>=J2.getHitbox().getB()))))
-	);
+	); 
 }
 
 
