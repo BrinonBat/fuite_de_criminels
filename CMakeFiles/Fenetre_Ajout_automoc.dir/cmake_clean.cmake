edf53fa50c1e7ca0d94a0a6b990febd865293202
BrinonBat <baptiste@brinon.eu>
2020-05-02 13:13:10 +0200

    changements gitignore

--- CMakeFiles/Fenetre_Ajout_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Fenetre_Ajout_automoc.cpp"
-  "CMakeFiles/Fenetre_Ajout_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Fenetre_Ajout_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
