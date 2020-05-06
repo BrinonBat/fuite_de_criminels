909d13c877dff8458282ce80831a80a2c632d696
BrinonBat <baptiste@brinon.eu>
2020-05-07 01:41:44 +0200

    ajout d'un fichier pour la génération automatique de documentation

---
+++ Source/Docu_generator/DocEntites.hh
@@ -0,0 +1,22 @@
+/** \file Entites.hh
+*	\brief contient les classes présentes sur le terrain, qu'il s'agisse de joueurs ou d'objets.
+*	\note ce fichier contient aussi les constantes et variables de classes nécessaires au fonctionnement des classes associées
+*/
+
+/** \def TAILLE_HITBOX
+*	\brief constante déterminant le diamètre des entités
+*	\see Entites
+*/
+
+/** \def TAILLE_TERRAIN
+*	\brief 	constante déterminant la taille du terrain utilisé durant la partie.
+*			Elle est notamment utilisée pour définir le cadre du jeu;
+*/
+
+
+
+
+
+/** \fn void Entite::setHitbox()
+* 	\sa TAILLE_HITBOX
+*/
