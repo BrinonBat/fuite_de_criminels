8f9e8e40713b0eabe88bca1d80ad5a0f74fdf15d
BrinonBat <baptiste@brinon.eu>
2020-05-18 19:47:43 +0200

    ajout de commentaires pour qu'on comprenne un truc à ce bordel

--- Source/Fenetre_Ajout.cc
+++ Source/Fenetre_Ajout.cc
@@ -8,25 +8,31 @@ Fenetre_Ajout::Fenetre_Ajout()
 	// Ajout de Joueurs
 	_Intro = new QLabel("	Veuillez choisir un type de joueurs, son nom, sa position et son algo:\n 	(Nom attaché de préférence comme G1,G2 ..)",this);
 
+	//selection du rôle
 	_ChoixJoueur = new QComboBox(this);
 	_ChoixJoueur->addItem("Voleur",0);
 	_ChoixJoueur->addItem("Gendarme",1);
 	_ChoixJoueur->setGeometry(0,40,100,20);
 
+	//saisie du nom du joueur
 	_NomJoueur = new QLineEdit("Nom Joueur",this);
 	_NomJoueur->setGeometry(100,40,100,20);
 
+	//saisie de la Position
 	_PositionX = new QLineEdit("Position X",this);
 	_PositionX->setGeometry(200,40,100,20);
 	_PositionY = new QLineEdit("Position Y",this);
 	_PositionY->setGeometry(300,40,100,20);
 
+	//selection de l'IA
 	_ChoixAlgo = new QComboBox(this);
 	_ChoixAlgo->addItem("Random",0);
 	_ChoixAlgo->addItem("Gendarme Chasseur",1);
 	_ChoixAlgo->addItem("IA Facile",2);
+	_ChoixAlgo->addItem("IA Moyenne",3);
 	_ChoixAlgo->setGeometry(400,40,140,20);
 
+	//bouton de validation
 	_Ajouter = new QPushButton("Ajouter",this);
 	_Ajouter->setGeometry(540,40,60,20);
 	connect(_Ajouter,SIGNAL(released()),this,SLOT(Ajouter_Joueur()));
@@ -35,37 +41,43 @@ Fenetre_Ajout::Fenetre_Ajout()
 	_Intro2 = new QLabel("	Veuillez choisir un type(Sortie,Cachette),sa position :\n ",this);
 	_Intro2->move(0,65);
 
+	//selection du type de NonJoueur
 	_AutreChoix = new QComboBox(this);
 	_AutreChoix->addItem("Sortie",0);
 	_AutreChoix->addItem("Cachette",1);
 	_AutreChoix->setGeometry(0,100,100,20);
 
+	//saisie de la Position
 	_PositionX2 = new QLineEdit("Position X",this);
 	_PositionX2->setGeometry(100,100,100,20);
 	_PositionY2 = new QLineEdit("Position Y",this);
 	_PositionY2->setGeometry(200,100,100,20);
 
+	//bouton de validation
 	_Ajouter2 = new QPushButton("Ajouter",this);
 	_Ajouter2->setGeometry(300,100,60,20);
 	connect(_Ajouter2,SIGNAL(released()),this,SLOT(Ajouter_NonJoueur()));
 
-	// Lancement de partie
+	//bouton de lancement de partie
 	_Lancer = new QPushButton("Lancer la partie!",this);
 	_Lancer->setGeometry(150,150,150,50);
 	connect(_Lancer,SIGNAL(released()),this,SLOT(Lancer_Partie()));
 
+	//bouton d'affichage de la fenêtre de la partie en cours
 	_Apercu = new QPushButton("Apercu",this);
 	_Apercu->setGeometry(300,150,150,50);
 	connect(_Apercu,SIGNAL(released()),this,SLOT(Apercu()));
 
+	//bouton d'interruption de partie en cours
 	_FinPartie = new QPushButton("Fin Partie",this);
 	_FinPartie->setGeometry(450,150,150,50);
 	connect(_FinPartie,SIGNAL(released()),this,SLOT(Fin()));
 
-	// Partie configuré - exemple (3)
-	_Intro3 = new QLabel("	Sinon vous pouvez lancer une partie avec\n\t3 exemples déja configuré :\n ",this);
+	// Parties préconfigurées - exemples
+	_Intro3 = new QLabel("	Sinon vous pouvez lancer une partie avec\n ces exemples déja configuré :\n ",this);
 	_Intro3->move(0,220);
 
+	//selection de l'exemple à jouer
 	_ChoixExemple = new QComboBox(this);
 	_ChoixExemple->addItem("Exemple1",0);
 	_ChoixExemple->addItem("Exemple2",1);
@@ -74,6 +86,7 @@ Fenetre_Ajout::Fenetre_Ajout()
 	_ChoixExemple->addItem("Exemple5",4);
 	_ChoixExemple->setGeometry(0,280,100,20);
 
+	//bouton de validation
 	_Lancer2 = new QPushButton("Lancer la partie!",this);
 	_Lancer2->setGeometry(100,280,150,20);
 	connect(_Lancer2,SIGNAL(released()),this,SLOT(Choix_Exemple()));
@@ -81,6 +94,7 @@ Fenetre_Ajout::Fenetre_Ajout()
 	_Liste = new QLabel("",this);
 	_Liste->setGeometry(0,310,600,300);
 
+	//lancement de la partie
 	Game = new Jeu();
 	Fenetre_Game = new Fenetre_Jeu();
 
@@ -89,8 +103,10 @@ Fenetre_Ajout::Fenetre_Ajout()
 /// Ajout d'un joueur dans le JEU et modification du Label sur la fênetre de configuration pour voir les ajouts
 void Fenetre_Ajout::Ajouter_Joueur(){
 	switch (_ChoixJoueur->currentIndex()){
-    	case 0:{
-			switch(_ChoixAlgo->currentIndex()){
+
+		case 0:{ // cas de l'ajout d'un voleur
+
+			switch(_ChoixAlgo->currentIndex()){ // cas correspondant aux différentes IA
         		case 0:{
         			_Liste->setText(_Liste->text() + "Voleur:"+_NomJoueur->text()+"("+_PositionX->text()+","+_PositionY->text()+") Déplacement : Random\n");
 					this->Game->ajouter_Voleur(Voleur(Position(_PositionX->text().toDouble(),_PositionY->text().toDouble()),1.0,_NomJoueur->text().toStdString(),Choix_Algo::random));
@@ -105,11 +121,18 @@ void Fenetre_Ajout::Ajouter_Joueur(){
         			_Liste->setText(_Liste->text() + "Voleur:"+_NomJoueur->text()+"("+_PositionX->text()+","+_PositionY->text()+") Déplacement : IA Facile\n");
 					this->Game->ajouter_Voleur(Voleur(Position(_PositionX->text().toDouble(),_PositionY->text().toDouble()),1.0,_NomJoueur->text().toStdString(),Choix_Algo::IA_Facile));
         		}break;
+
+				case 3:{
+        			_Liste->setText(_Liste->text() + "Voleur:"+_NomJoueur->text()+"("+_PositionX->text()+","+_PositionY->text()+") Déplacement : IA Moyenne\n");
+					this->Game->ajouter_Voleur(Voleur(Position(_PositionX->text().toDouble(),_PositionY->text().toDouble()),1.0,_NomJoueur->text().toStdString(),Choix_Algo::IA_Moyen));
+        		}break;
         	}
+
         }break;
 
-        case 1:{
-        	switch(_ChoixAlgo->currentIndex()){
+        case 1:{ // cas de l'ajout d'un Gendarme
+
+        	switch(_ChoixAlgo->currentIndex()){// cas correspondant aux différentes IA
         		case 0:{
         			_Liste->setText(_Liste->text() + "Gendarme:"+_NomJoueur->text()+"("+_PositionX->text()+","+_PositionY->text()+") Déplacement : Random\n");
 					this->Game->ajouter_Gendarme(Gendarme(Position(_PositionX->text().toDouble(),_PositionY->text().toDouble()),1.0,_NomJoueur->text().toStdString(),Choix_Algo::random));
@@ -124,29 +147,36 @@ void Fenetre_Ajout::Ajouter_Joueur(){
         			_Liste->setText(_Liste->text() + "Gendarme:"+_NomJoueur->text()+"("+_PositionX->text()+","+_PositionY->text()+") Déplacement : IA Facile\n");
 					this->Game->ajouter_Gendarme(Gendarme(Position(_PositionX->text().toDouble(),_PositionY->text().toDouble()),1.0,_NomJoueur->text().toStdString(),Choix_Algo::IA_Facile));
         		}break;
+
+				case 3:{
+        			_Liste->setText(_Liste->text() + "Gendarme:"+_NomJoueur->text()+"("+_PositionX->text()+","+_PositionY->text()+") Déplacement : IA Moyenne\n");
+					this->Game->ajouter_Gendarme(Gendarme(Position(_PositionX->text().toDouble(),_PositionY->text().toDouble()),1.0,_NomJoueur->text().toStdString(),Choix_Algo::IA_Moyen));
+        		}break;
         	}
+
         }break;
+
     }
 }
 
 /// Ajout d'un Non_Joueur dans le JEU et modification du Label sur la fênetre de configuration pour voir les ajouts
 void Fenetre_Ajout::Ajouter_NonJoueur(){
+
 	switch (_AutreChoix->currentIndex()){
-        case 0:{
-            // Voleur V1(Position(_PositionX->text().toDouble(),_PositionY->text().toDouble()),1.0,"V1");
+
+        case 0:{ // cas de l'ajout d'une Sortie
              _Liste->setText(_Liste->text() + "Sortie: ("+_PositionX2->text()+","+_PositionY2->text()+")\n");
 			this->Game->ajouter_nonJoueur(NonJoueur(Position(_PositionX2->text().toDouble(),_PositionY2->text().toDouble()),Type::sortie));
         }break;
 
-        case 1:{
+        case 1:{ // cas de l'ajout d'une Cachette
         	// Pas encore de gestion de cachette.
 
         }break;
+
     }
 
 }
-
-
 
 /// Ajout de la couleur de l'item Rect de chaque entité et ajout sur la scène.
 void Fenetre_Ajout::Configuration_Partie(){
@@ -210,6 +240,7 @@ void Fenetre_Ajout::Jouer_Partie(){
 	Fenetre_Game = new Fenetre_Jeu();
 
 }
+
 /// Fenetre d'apercu avant lancement de partie
 void Fenetre_Ajout::Apercu(){
 	Fenetre_Game->fenetre_apercu = true; // Pour gérer la fermeture de la fenetre Apercu, vue que le jeu une fois lancé si on quitte, arrête la partie
@@ -221,15 +252,22 @@ void Fenetre_Ajout::Apercu(){
 /// Fonction qui permet d'écrire les resultats de la partie une fois terminés dans "Resultat.txt"
 void Fenetre_Ajout::Ecriture_Resultats(int duree){
 
-	//déclaration
+	//ouverture du fichier
 	std::ofstream Resultats;
 
+	//enregistrement des données dans le fichier
 	Resultats.open ("Resultats.txt",std::ios::app);
-	Resultats << "------------------------------\n Résultat de la partie : \n Temps : "<<
-	std::to_string(duree)<<" secondes \n Nombre de voleur(s): "<<std::to_string(Game->getNbVoleurs())<<"\n Nombre de gendarme(s): "<<std::to_string(Game->getNbGendarmes())<<"\nVoleur(s) Capturé(s) : "<<std::to_string(Game->getNbCaptures())<<
-	" sur "<<std::to_string(Game->getNbVoleurs())<<" \n Voleur(s) Sortie(s) :"<<std::to_string(Game->getNbVoleursSortis())<<" sur "<<std::to_string(Game->getNbVoleurs())<<
-	" \n Algorithme utilisé : AFAIRE \n Autres informations : AFAIRE \n------------------------------\n";
+	Resultats << "------------------------------\n
+	Résultat de la partie : \n
+	Temps : "<<std::to_string(duree)<<" secondes \n
+	Nombre de voleur(s): "<<std::to_string(Game->getNbVoleurs())<<"\n
+	Nombre de gendarme(s): "<<std::to_string(Game->getNbGendarmes())<<"\n
+	Voleur(s) Capturé(s) : "<<std::to_string(Game->getNbCaptures())<<" sur "<<std::to_string(Game->getNbVoleurs())<<" \n
+	Voleur(s) Sorti(s) :"<<std::to_string(Game->getNbVoleursSortis())<<" sur "<<std::to_string(Game->getNbVoleurs())<<" \n
+	"//"Autres informations : AFAIRE \n"<<
+	"------------------------------\n";
 
+	//fermeture du fichier
 	Resultats.close();
 }
 
@@ -247,9 +285,14 @@ void Fenetre_Ajout::Lancer_Partie(){
 
 /// Bouton de Fin de partie et reset
 void Fenetre_Ajout::Fin(){
+
+	//suppression du Jeu et de la fenetre
 	delete Game;
 	delete Fenetre_Game;
+
+	//réinitialisation
 	Game = new Jeu();
 	Fenetre_Game = new Fenetre_Jeu();
 	_Liste->setText("");
+
 }
